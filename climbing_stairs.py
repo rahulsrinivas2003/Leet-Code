04efa6fa3ee292924541d08d7d8cb61d8f1c786e
Rahul Srinivas Selvaraju <rahulsrinivasselvaraju@gmail.com>
2026-04-04 18:52:54 +1100

    Leet Code Climbing stairs completed

---
+++ climbing_stairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        
+        memo = { 1:1 , 2:2 , 3:3}
+
+        def f(n):
+
+            if n in memo:
+
+                return memo[n]
+            else:
+
+                memo[n] = f(n-2) + f(n-1)
+                return memo[n]
+        
+        return f(n)
+        
